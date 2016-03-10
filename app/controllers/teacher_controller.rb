7e2d73128a517b4e1b959fd315cb9689dfb23a86
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-03-09 19:23:46 -0500

    Teachers now only see parents of their students

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -6,7 +6,8 @@ class TeacherController < ApplicationController
     @users_teachers = User.with_group(group: :teacher)
     teacher_id = Teacher.find_by(user_id: session[:user_id]).id
     @users_students = User.with_group(group: :student, clause: "teacher_id = #{teacher_id}")
-    @users_parents = User.with_group(group: :parent)
+    @users_parents = User.joins("JOIN parents ON users.id = parents.user_id JOIN students ON parents.student_id = students.id JOIN teachers ON students.teacher_id = #{teacher_id}").group(:email)
+    # byebug
     @assignments = Assignment.all
   end
 
